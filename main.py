7d7d3edd2347a3c40aba6443e24540df91cd0ef2
Dawid Kolodziej <dawid_kolodziej@o2.pl>
2024-10-28 13:59:41 +0100

    sums

--- main.py
+++ main.py
@@ -1,61 +1,99 @@
 import re
 import os
 from openpyxl import load_workbook
+from openpyxl.utils import rows_from_range
 
 # Define Input and output paths below
-path = "C:\\Users\\Dawid.Kolodziej1\\OneDrive - Precisely Inc\\Desktop\\000"
-path_out = "C:\\Users\\Dawid.Kolodziej1\\OneDrive - Precisely Inc\\Desktop\\001"
+path = "C:\\test\\000"
+path_out = "C:\\test\\001"
 
-for File in os.listdir(path + "\\" + "."):
-    if File.endswith(".txt"):
+# creates list of files in the path directory
+filelist = os.listdir(path)
 
-        fileList = os.listdir(path)[0]
+# empty list to store the txt files
+txt_files = []
 
-        x = re.findall("[0-9]", fileList)
-        x.insert(4, "-")
-        x.insert(7, "-")
+# loop to go through files in the directory
+for file in filelist:
+    # check if the file has a txt extension
+    if file.endswith(".txt"):
+        # adding txt files to the list
+        txt_files.append(file)
 
 
-        date = "".join(x)
-        sheet_name = date[0:7]
+# loop to go through txt filenames in the list and create a sheetname [date]
+        for txt_file in txt_files:
+            x = re.findall("[0-9]", txt_file)
+            x.insert(4, "-")
+            x.insert(7, "-")
+
+            date = "".join(x)
+            sheet_name = date[0:7]
 
 
-        f = open(path + "\\" + fileList, "r")
-        files = f.read()
-        pattern = ["P1", "P2", "P3", "P4"]
-        p_table = []
-        for p in pattern:
-            match_p = re.findall(p, files)
-            p_table.append(len(match_p))
+            # searching for pattern in the txt files and counting them
+            f = open(path + "\\" + txt_file, "r")
+            files = f.read()
+            pattern = ["P1", "P2", "P3", "P4"]
+            p_table = []
+            for p in pattern:
+                match_p = re.findall(p, files)
+                p_table.append(len(match_p))
 
-        print(p_table)
+            f.close()
+        print(date, p_table)
+        myFileName = path_out + "\\" + "a.xlsx"
 
-        f.close()
-
-        myFileName = path_out + "\\" + "DemoFile2.xlsx"
-
-        #load the workbook, and put the sheet into a variable
+        # load the workbook, and put the sheet into a variable
         wb = load_workbook(filename=myFileName)
 
+        # creating a sheet in the workbook
         if not sheet_name in wb.sheetnames:
             wb.create_sheet(sheet_name)
 
         ws = wb[sheet_name]
 
-        #max_row is a sheet function that gets the last row in a sheet.
+        # max_row is a sheet function that gets the last row in a sheet.
         newRowLocation = ws.max_row + 1
 
-        #write to the cell you want, specifying row and column, and value :-)
+        # write to the cell you want, specifying row and column, and value :-)
         ws.cell(column=1, row=newRowLocation, value=date)
         ws.cell(column=2, row=newRowLocation, value=p_table[0])
         ws.cell(column=3, row=newRowLocation, value=p_table[1])
         ws.cell(column=4, row=newRowLocation, value=p_table[2])
         ws.cell(column=5, row=newRowLocation, value=p_table[3])
 
-
         wb.save(filename=myFileName)
         wb.close()
-    else:
-        print("No TXT file")
+myFileName = path_out + "\\" + "a.xlsx"
+sum_wb = load_workbook(filename=myFileName)
+list_of_sheets = sum_wb.sheetnames
 
-    os.rename(path + "\\" + fileList, path_out + "\\" + fileList)
+for sheet in list_of_sheets:
+    # if sheet == 'Sheet1':
+    #     del sum_wb['Sheet1']
+    worksheet = sum_wb[sheet]
+    rows_count = worksheet.max_row
+    column_count = worksheet.max_column
+
+
+    first_column = 2
+    last_row = rows_count
+    sum_row = last_row + 3
+    start_col = 2
+    end_col = column_count
+
+    for col in worksheet.iter_rows(min_row=sum_row, max_row=sum_row, min_col=start_col, max_col=end_col):
+        for cell in col:
+            cell_sum_start = cell.column_letter + str(first_column)
+            cell_sum_end = cell.column_letter + str(last_row)
+            cell.value = '=SUM({0}:{1})'.format(cell_sum_start, cell_sum_end)
+
+
+
+sum_wb.save(filename=myFileName)
+sum_wb.close()
+
+# moving files from in directory to out directory
+for x in filelist:
+    os.rename(path + "\\" + x, path_out + "\\" + x)
